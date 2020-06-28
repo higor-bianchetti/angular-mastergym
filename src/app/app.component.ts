4ebb0e57a7c8f14b158fcb17ec632c1c4f9dd765
Higor Bianchetti <higorbianchetti@gmail.com>
2020-06-28 18:35:49 -0300

    Testing firebase authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
+import { auth } from "firebase/app";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'mastergym';
+  title = "mastergym";
+
+  constructor(public afAuth: AngularFireAuth) {}
+
+  login() {
+    this.afAuth.auth.signInWithEmailAndPassword(
+      "higorbianchetti@gmail.com",
+      "123456"
+    );
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from "angularfire2";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { environment } from "../environments/environment";
+import { AngularFireAuth } from "angularfire2/auth";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +13,7 @@ import { environment } from "../environments/environment";
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [],
+  providers: [AngularFireAuth],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
