0702eec113d5e51e49a26c516770507ec5b31083
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-01 19:22:44 -0300

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
 import { AngularFireModule } from "angularfire2";
+
+import { environment } from "../environments/environment";
+import { LoginComponent } from "./login/login.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { environment } from "../environments/environment";
-import { AngularFireAuth } from "angularfire2/auth";
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [AngularFireAuth],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AngularFireAuth } from "angularfire2/auth";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  formLogin: FormGroup;
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.formLogin = this.formBuilder.group({
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+      password: ["", Validators.required],
+    });
   }
 
+  login() {
+    this.afAuth.auth
+      .signInWithEmailAndPassword(
+        this.formLogin.value.email,
+        this.formLogin.value.password
+      )
+      .then((user) => {
+        console.log("USER: ", user);
+      });
+  }
 }
