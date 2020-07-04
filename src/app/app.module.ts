3134cb76143f2bf79441f76e7b1d6e075aa7dfb5
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-04 15:54:31 -0300

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,28 @@ import { NgModule } from "@angular/core";
 import { AngularFireAuth } from "angularfire2/auth";
 import { AngularFireModule } from "angularfire2";
 import { NgxSpinnerModule } from "ngx-spinner";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { environment } from "../environments/environment";
 import { LoginComponent } from "./login/login.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoadingComponent } from "./ui/loading/loading.component";
+import { HeaderComponent } from "./ui/header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LoadingComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LoadingComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     NgxSpinnerModule,
+    NgbDropdownModule,
     AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [AngularFireAuth],
