4b3f6bf9c1de7338a73cb1cd1cecb80adc1d34c2
Higor Bianchetti <higorbianchetti@gmail.com>
2020-07-11 19:13:00 -0300

    Customer form validation

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-new-customer',
-  templateUrl: './new-customer.component.html',
-  styleUrls: ['./new-customer.component.scss']
+  selector: "app-new-customer",
+  templateUrl: "./new-customer.component.html",
+  styleUrls: ["./new-customer.component.scss"],
 })
 export class NewCustomerComponent implements OnInit {
+  customerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.customerForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+      cpf: ["", Validators.required],
+      birth: ["", Validators.required],
+      phone: [""],
+      img: ["", Validators.required],
+    });
   }
 
+  add() {
+    console.log(this.customerForm.value);
+  }
 }
